b38fb1cd1adfad1e701a5414a0e6d038a9c47063
Алексей Бельченков <u608110@gmail.com>
2023-04-18 20:35:24 +0300

    Output all connected ports - type, name

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 use serialport::*;
 
 fn main() {
+    let mut last_port_count = 0;
+
     match available_ports() {
-       Ok(ports) => {
-            for port in ports {
-                println!("PORTS {:?}", port);
-                println!("----------------");
-                println!("PORTS {:?}", port.port_type);
+       Ok(port_infos) => {
+            let curr_port_count = port_infos.len();
+
+            if curr_port_count != last_port_count {
+                last_port_count = curr_port_count;
+                println!("last_port_count: {}", last_port_count);
+
+                for port_info in port_infos {
+                    println!("Type: {:?}", port_info.port_type);
+                    println!("Name: {:?}", port_info.port_name);
+                }
+
             }
        },
        Err(err_var) => println!("Err {:?}", err_var)
